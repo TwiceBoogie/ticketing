ce819f73789d9fdad9ce743e5ef2cfdf2e4e71c8
Sebastian <salsebastian13@gmail.com>
2023-11-16 10:32:18 -0600

    Updates

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -8,7 +8,7 @@ export interface OrderCreatedEvent {
     version: number;
     status: OrderStatus;
     userId: string;
-    userEmail: string;
+    sessionId: string;
     expiresAt: string;
     ticket: {
       id: string;
