6a5b891846c6c77df0b92e0ffd316eecb3085397
Sebastian <salsebastian13@gmail.com>
2025-02-27 13:15:40 -0600

    Updates

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -1,13 +1,12 @@
 import { Request, Response, NextFunction } from "express";
-
 import { CustomError } from "../errors/custom-error";
 
-export const errorHandler = (
+export const errorHandler: (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+) => void = (err, req, res, next) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
